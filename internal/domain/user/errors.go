253a049816a2d940595a85ebc244b5fa4529f6a2
Peshawa Aziz <modern3p2017@gmail.com>
2026-07-17 20:29:47 +0330

    feat: add user errors

--- internal/domain/user/errors.go
+++ internal/domain/user/errors.go
@@ -3,6 +3,9 @@ package user
 import "errors"
 
 var (
-	ErrUserNotFound      = errors.New("user not found")
-	ErrEmailAlreadyTaken = errors.New("email already taken for this tenant")
+	ErrUserNotFound       = errors.New("user not found")
+	ErrEmailAlreadyTaken  = errors.New("email already taken for this tenant")
+	ErrInvalidCredentials = errors.New("invalid email or password")
+	ErrAccountLocked      = errors.New("account temporarily locked")
+	ErrEmailNotVerified   = errors.New("email not verified")
 )
